beffca7384a52326da1e584deaf5611dc2062c4f
Ahsaan Kumar <108061867+Ahsaan02@users.noreply.github.com>
2024-01-22 12:05:09 +0000

    parent classes and navigation bar
    parent classes (HTML, CSS) and a responsive navigation bar

--- pdfdocumentgenerator/pdfinstant/urls.py
+++ pdfdocumentgenerator/pdfinstant/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path(route="", view=views.index, name="Home"),
-    path(route="second", view=views.second, name="second"),
-    path(route="", view=views.index, name="homepage")
+    path(route="index", view=views.index, name="index"),
+
+    path(route="aboutpage", view=views.aboutpage, name="aboutpage"),
+    path(route="homepage", view=views.homepage, name="homepage"),
+    path(route="generatepdfs", view=views.generatepdfs, name="generatepdfs"),
+    path(route="signin", view=views.signin, name="signin"),
+    path(route="register", view=views.register, name="register")
 ]
--- pdfdocumentgenerator/pdfinstant/views.py
+++ pdfdocumentgenerator/pdfinstant/views.py
@@ -6,11 +6,28 @@ from django.http import HttpResponse
 # def index(request):
 #     return HttpResponse("This is the PDF Instant App.\n<h1>To Goto the second page click <a href='/second'>here</a></h1>")
 
+
 def index(request):
+    page = loader.get_template("pdfinstant/index.html")
+    return HttpResponse(page.render(request=request))
+
+def homepage(request):
     page = loader.get_template("pdfinstant/homepage.html")
     return HttpResponse(page.render(request=request))
 
 
-def second(request):
-    page = loader.get_template("pdfinstant/second.html")
+def aboutpage(request):
+    page = loader.get_template("pdfinstant/aboutpage.html")
+    return HttpResponse(page.render(request=request))
+
+def generatepdfs(request):
+    page = loader.get_template("pdfinstant/generatepdfs.html")
+    return HttpResponse(page.render(request=request))
+
+def signin(request):
+    page = loader.get_template("pdfinstant/signin.html")
+    return HttpResponse(page.render(request=request))
+
+def register(request):
+    page = loader.get_template("pdfinstant/register.html")
     return HttpResponse(page.render(request=request))
