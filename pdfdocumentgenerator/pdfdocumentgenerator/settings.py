504dd4829c2e9b8c3073bfdb82accfd4fe444b3b
Ahsaan Kumar <108061867+Ahsaan02@users.noreply.github.com>
2024-03-28 23:02:18 +0000

    Modal popup for the optionality to have generated PDFs emailed or downloaded collectively in ZIP format

--- pdfdocumentgenerator/pdfdocumentgenerator/settings.py
+++ pdfdocumentgenerator/pdfdocumentgenerator/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,6 +95,15 @@ DATABASES = {
         'PORT':'3306'
     }
 }
+
+
+# Email configurations
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- pdfdocumentgenerator/pdfinstant/static/pdfinstant/template_choices.js
+++ pdfdocumentgenerator/pdfinstant/static/pdfinstant/template_choices.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const radios = document.querySelectorAll('input[type="radio"][name="template"]');
     radios.forEach(radio => {
@@ -6,5 +5,51 @@ document.addEventListener('DOMContentLoaded', () => {
             radios.forEach(radio => radio.closest('.responsive-box').classList.remove('selected'));
             radio.closest('.responsive-box').classList.add('selected');
         });
+    });
+
+    const generatePdfBtn = document.getElementById('generatePdfBtn');
+    const modal = document.getElementById('pdfActionModal');
+    const span = document.getElementsByClassName("close")[0];
+    const emailPdfsBtn = document.getElementById('emailPdfs');
+    const downloadZipBtn = document.getElementById('downloadZip');
+    const pdfForm = document.getElementById('pdfForm');
+
+    generatePdfBtn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    function removeActionInput() {
+        const existingActionInput = pdfForm.querySelector('input[name="action"]');
+        if (existingActionInput) {
+            pdfForm.removeChild(existingActionInput);
+        }
+    }
+
+    emailPdfsBtn.addEventListener('click', () => {
+        removeActionInput();
+
+        const emailActionInput = document.createElement('input');
+        emailActionInput.setAttribute('type', 'hidden');
+        emailActionInput.setAttribute('name', 'action');
+        emailActionInput.setAttribute('value', 'emailPdfs');
+        pdfForm.appendChild(emailActionInput);
+
+        pdfForm.submit();
+    });
+
+    downloadZipBtn.addEventListener('click', () => {
+        removeActionInput();
+
+        pdfForm.submit();
     });
 });
